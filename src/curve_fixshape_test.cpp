b32a2b249bc9f596602e063184a697168bc7357b
joshia5 <joshia5@rpi.edu>
2025-09-04 12:15:52 -0400

    setup shock on cyl grv case

--- src/curve_fixshape_test.cpp
+++ src/curve_fixshape_test.cpp
@@ -3,9 +3,84 @@
 #include <Omega_h_build.hpp>
 #include <Omega_h_curve_validity_3d.hpp>
 #include <Omega_h_file.hpp>
+#include <Omega_h_for.hpp>
 #include <Omega_h_library.hpp>
+#include <Omega_h_metric.hpp>
+#include <Omega_h_timer.hpp>
 
 using namespace Omega_h;
+
+template <Int dim>
+static void set_target_metric(Mesh* mesh) {
+  auto coords = mesh->coords();
+  auto target_metrics_w = Write<Real>(mesh->nverts() * symm_ncomps(dim));
+  auto f = OMEGA_H_LAMBDA(LO v) {
+    auto y = coords[v * dim + (dim - 2)];
+    auto h = Vector<dim>();
+    h[0] = 0.1; h[1] = 0.1; h[2] = 0.1;
+    //for (Int i = 0; i < dim - 1; ++i) h[i] = 0.1;
+    h[dim - 2] = 0.001 + 0.198 * std::abs(y - 0.5);
+    auto m = diagonal(metric_eigenvalues_from_lengths(h));
+    set_symm(target_metrics_w, v, m);
+  };
+  parallel_for(mesh->nverts(), f);
+  mesh->set_tag(VERT, "target_metric", Reals(target_metrics_w));
+}
+
+template <Int dim>
+void run_case(Mesh* mesh, char const* vtk_path) {
+  auto world = mesh->comm();
+  mesh->set_parting(OMEGA_H_GHOSTED);
+  auto implied_metrics = get_implied_metrics(mesh);
+  mesh->add_tag(VERT, "metric", symm_ncomps(dim), implied_metrics);
+  mesh->add_tag<Real>(VERT, "target_metric", symm_ncomps(dim));
+  set_target_metric<dim>(mesh);
+  mesh->set_parting(OMEGA_H_ELEM_BASED);
+  mesh->ask_lengths();
+  mesh->ask_qualities();
+  vtk::FullWriter writer;
+  if (vtk_path) {
+    writer = vtk::FullWriter(vtk_path, mesh);
+    writer.write();
+  }
+  auto opts = AdaptOpts(mesh);
+  opts.verbosity = EXTRA_STATS;
+  opts.length_histogram_max = 2.0;
+  opts.max_length_allowed = opts.max_length_desired * 2.0;
+  opts.should_smooth_snap = 0;
+  opts.should_coarsen = 0;
+  opts.should_swap = 0;
+  opts.should_coarsen_slivers = 0;
+  opts.check_crv_qual = 0;
+  opts.min_quality_allowed = 0.000001;
+  opts.min_quality_desired = 0.1;
+  Now t0 = now();
+  auto desired_group_nelems = 2000;
+  while (approach_metric(mesh, opts)) {
+    auto nelems = mesh->nglobal_ents(mesh->dim());
+    if (nelems < 8000) adapt(mesh, opts);
+    else break;
+    if (mesh->has_tag(VERT, "target_metric")) set_target_metric<dim>(mesh);
+    /*
+    auto wireframe_mesh = Mesh(comm->library());
+    wireframe_mesh.set_comm(comm);
+    build_cubic_wireframe_3d(&mesh, &wireframe_mesh, 10);
+    std::string vtuPath = 
+      "/lore/joshia5/Meshes/curved/Kova-123tetwire_ansiov2.vtu";
+    vtk::write_simplex_connectivity(vtuPath.c_str(), &wireframe_mesh, 1);
+
+    auto curveVtk_mesh = Mesh(comm->library());
+    curveVtk_mesh.set_comm(comm);
+    build_cubic_curveVtk_3d(&mesh, &curveVtk_mesh, 10);
+    vtuPath = 
+      "/lore/joshia5/Meshes/curved/Kova-123tetcurveVtk_anisov2.vtu";
+    vtk::write_simplex_connectivity(vtuPath.c_str(), &curveVtk_mesh, 2);
+    //if (vtk_path) writer.write();
+    */
+  }
+  Now t1 = now();
+  std::cout << "total time: " << (t1 - t0) << " seconds\n";
+}
 
 void test_cyl_grv(Library *lib) {
   auto comm = lib->world();
@@ -31,13 +106,15 @@ void test_cyl_grv(Library *lib) {
   mesh.set_tag(VERT, "metric", Reals(mesh.nverts(), 1));
   auto valid_tris_bef = checkValidity_3d(&mesh, LOs(mesh.nregions(), 0, 1));
   auto qual = calc_crvQuality_3d(&mesh);
+  run_case<3>(&mesh, NULL);
+  /*
   //auto quals = askQuality_2d(&mesh, LOs(mesh.nfaces(), 0, 1), 2);
   for (LO adapt_itr = 0; adapt_itr < 1; ++adapt_itr) {
     fprintf(stderr, "itr %d\n", adapt_itr);
     adapt(&mesh, opts);
   }
+  */
 
-  /*
   auto wireframe_mesh = Mesh(lib);
   wireframe_mesh.set_comm(comm);
   build_cubic_wireframe_2d(&mesh, &wireframe_mesh, 4);
@@ -51,14 +128,6 @@ void test_cyl_grv(Library *lib) {
     "/users/joshia5/lore.scorec.rpi.edu/Meshes/curved/cyl_grv_shock.vtu";
   vtk::write_simplex_connectivity(vtuPath.c_str(), &cubic_curveVtk_mesh, 2);
   auto valid_tris_aft = checkValidity_3d(&mesh, LOs(mesh.nregions(), 0, 1));
-  //quals = askQuality_2d(&mesh, LOs(mesh.nfaces(), 0, 1), 2);
-
-  vtk::FullWriter writer;
-  writer = vtk::FullWriter(
-      "/lore/joshia5/Meshes/curved/annulus-8-swap_full.vtk", &mesh);
-  writer.write();
-  */
-  //mesh.ask_qualities();
 
   return;
 }
